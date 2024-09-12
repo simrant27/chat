f5a59b431a5c2afcc33ef1f5b4a6069c93cc5c44
Simran Tamang <simrant009@gmail.com>
2024-09-12 17:26:33 +0545

    first commit

---
+++ frontend/chat/lib/Screens/chatscreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Chatscreen extends StatelessWidget {
+  const Chatscreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Chats"),
+      ),
+    );
+  }
+}
