06c5183a5b36c302bd7be3670a150429158a94ef
Simran Tamang <simrant009@gmail.com>
2024-09-12 18:47:19 +0545

    frontend

---
+++ frontend/chat/lib/Screens/homescreen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade200,
+        title: Text(
+          "Chats",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.more_vert))
+        ],
+      ),
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
--- frontend/chat/lib/main.dart
+++ frontend/chat/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:chat/Screens/chatscreen.dart';
+import 'package:chat/Screens/homescreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Chatscreen(),
+      home: HomeScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
